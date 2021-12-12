ab2a90d51f220ce4440de3548034c14cf49c771a
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2021-12-12 18:52:03 +0530

    pathfinding

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const $ = (id) => document.getElementById(id);
 
 let hash_code = 0;
 let id;
+const dirs = generateDirs();
 
 init();
 function init() {
@@ -15,26 +16,59 @@ function init() {
   id = setInterval(() => {
     for (const i in eles) {
       const copy = eles.slice();
-
-      const ele = eles[i];
-      ele.move(ele.vel);
-      ele.vel = randomVel();
-      ele.style["border-color"] = "#bbb";
-
       copy.splice(i, 1);
-      for (const against of copy) {
-        let result = checkCollision(ele,against);
-        if (result) {
-          ele.style["border-color"] = "green";
-          result = checkCollision(ele.childNodes[0],against);
-          ele.style["border-color"] = "green";
-          if (result) {
-            ele.style["border-color"] = "red";
-          }
-        }
-      }
+      frame(eles[i], copy);
     }
   }, 200);
+}
+
+function frame(ele, copy) {
+  ele.style["border-color"] = "#bbb";
+
+  // check collision
+  for (let against of copy) {
+    against = against.childNodes[0];
+    const result = checkCollision(ele, against);
+    if (result) {
+      ele.style["border-color"] = "green";
+      ele.following = against;
+    }
+  }
+
+  // follow
+  if (ele.following) {
+    ele.vel = bestDir(
+      ele.childNodes[0].getBoundingClientRect(),
+      ele.following.getBoundingClientRect()
+    );
+    ele.move(ele.vel);
+    return;
+  }
+
+  // move randomly
+  ele.vel = randomVel();
+  ele.move(ele.vel)
+}
+
+function bestDir(pos, goal) {
+  let best = Infinity;
+  let bestDir;
+
+  for (const dir of dirs) {
+    // add dir and pos
+    const newPos = {
+      x: pos.x + dir.x,
+      y: pos.y + dir.y,
+    };
+
+    const distance = dist(newPos, goal);
+    if (best > distance) {
+      best = distance;
+      bestDir = dir;
+    }
+  }
+
+  return bestDir;
 }
 
 function createBody() {
@@ -46,15 +80,17 @@ function createBody() {
   map.setAttribute("id", hash_id);
   map.x = random(10, 90);
   map.y = random(10, 90);
+  map.following = null;
   map.vel = randomVel();
 
-  map.move = ({ x, y }) => {
+  map.move = ({ x = 0, y = 0 }) => {
     map.x += x;
     map.y += y;
-    map.style.top = map.x + "%";
-    map.style.left = map.y + "%";
+    map.style.top = map.y + "%";
+    map.style.left = map.x + "%";
   };
 
+  map.move({});
   head.classList.add("head");
   map.appendChild(head);
   map.classList.add("creature");
@@ -70,27 +106,8 @@ function checkCollision(a, b) {
     rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x;
   const isInVerticalBounds =
     rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
-  const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
-  return isOverlapping;
+  return isInHoriztonalBounds && isInVerticalBounds;
 }
-
-/*
-function checkCollision(ele, func) {
-  const rect = ele.getBoundingClientRect();
-  const matches = document
-    .elementsFromPoint(rect.left, rect.top)
-    .filter((x) => ele.id != x.id && x.id);
-
-  if (!matches.length) log("null");
-
-  for (const match of matches) {
-    if (ele.id != match.id) {
-      log(ele.id + " --> " + match.id);
-      return match;
-    }
-  }
-}
-*/
 
 function randomVel() {
   return {
@@ -110,4 +127,22 @@ function hash() {
 
 function stop() {
   return clearInterval(id);
+}
+
+function dist(a, b) {
+  // distance formula
+  // √[(x2 − x1)2 + (y2 − y1)2]
+  const dist = Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2);
+  return Math.sqrt(dist);
+}
+
+function generateDirs() {
+  const dirs = [];
+  for (let x = -1; x != 2; x++) {
+    for (let y = -1; y != 2; y++) {
+      dirs.push({ x, y });
+    }
+  }
+  dirs.splice(4, 1);
+  return dirs;
 }
